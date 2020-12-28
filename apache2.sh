8eb211898a22af70dd787c1d0ee0acf4ed2e0ed9
Dhanaskv <dhanasekarkv1995@gmail.com>
2020-12-28 11:44:56 +0530

    Apache2 installation

---
+++ apache2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#Update the repository index pages........
+echo ">>>Updating the system repository index pages*********!!!!!!!!1"
+sudo apt update
+
+#Upgrading the already installed packages......
+echo ">>>Upgrading already installed packages************!!!!!!!!!!"
+sudo apt upgrade
+
+#Installing apache2 web server..........
+echo ">>>>Installing apache2 web server**********!!!!!!!!"
+sudo apt install apache2 -y
+
+#Start web server...............
+echo ">>>>Starting apache2 web server************!!!!!!!!"
+sudo systemctl start apache2
+
+#Enable the web server
+echo ">>>>Enabling the apache2 web server*********!!!!!!!!"
+sudo systemctl enable apache2
+
+#apache2 web server running status check...........
+echo ">>>Apache2 web server running status********!!!!!!!"
+sudo systemctl status apache2
